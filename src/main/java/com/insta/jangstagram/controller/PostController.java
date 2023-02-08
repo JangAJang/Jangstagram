213f2195f0b3fb885a58ab2d74e818f7bdae35dd
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-08 22:06:05 +0900

    feat: PostCreateRequestDto 게시글 생성 요청을 받아주는 dto 생성

--- src/main/java/com/insta/jangstagram/controller/PostController.java
+++ src/main/java/com/insta/jangstagram/controller/PostController.java
@@ -1,10 +1,14 @@
 package com.insta.jangstagram.controller;
 
+import com.insta.jangstagram.dto.PostCreateRequestDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class PostController {
 
     @GetMapping("/posts")
@@ -13,7 +17,8 @@ public class PostController {
     }
 
     @PostMapping ("/posts/add")
-    public String post(){
+    public String post(PostCreateRequestDto postCreateRequestDto){
+        log.info("params = {}", postCreateRequestDto.toString());
         return "Hello World";
     }
 }
---
+++ src/main/java/com/insta/jangstagram/dto/PostCreateRequestDto.java
@@ -0,0 +1,13 @@
+package com.insta.jangstagram.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(of = {"title", "content"})
+public class PostCreateRequestDto {
+
+    private String title;
+    private String content;
+}
