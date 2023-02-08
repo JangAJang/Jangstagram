57f40a70b49398f69a09a1799c8871060e9c3ea9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-08 21:54:32 +0900

    feat: PostControllerTest post요청을 처리하는 컨트롤러 기본 로직과 테스트코드 구현

--- src/main/java/com/insta/jangstagram/controller/PostController.java
+++ src/main/java/com/insta/jangstagram/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.insta.jangstagram.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,11 @@ public class PostController {
 
     @GetMapping("/posts")
     public String get(){
+        return "Hello World";
+    }
+
+    @PostMapping ("/posts/add")
+    public String post(){
         return "Hello World";
     }
 }
--- src/test/java/com/insta/jangstagram/controller/PostControllerTest.java
+++ src/test/java/com/insta/jangstagram/controller/PostControllerTest.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class PostControllerTest {
@@ -19,9 +23,19 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts를 요청하면 Hello World가 출력된다. ")
     public void getTest() throws Exception{
-        mvc.perform(MockMvcRequestBuilders.get("/posts"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello World"));
+        mvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+    @Test
+    @DisplayName("/posts/add를 요청하면 Hello World가 출력된다. ")
+    public void postTest() throws Exception{
+        mvc.perform(post("/posts/add")
+                        .param("title", "제목입니다")
+                        .param("content", "내용입니다"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andDo(print());
     }
 
 }
