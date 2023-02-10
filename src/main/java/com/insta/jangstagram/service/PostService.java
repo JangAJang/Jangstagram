7212e9cbae9a932ffd57003bafa0093034199879
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-10 22:26:06 +0900

    feat: PostService 비즈니스로직에서 입력을 받아 Post를 만들어 저장하는 기능 구현

--- src/main/java/com/insta/jangstagram/service/PostService.java
+++ src/main/java/com/insta/jangstagram/service/PostService.java
@@ -1,9 +1,21 @@
 package com.insta.jangstagram.service;
 
+import com.insta.jangstagram.domain.Post;
+import com.insta.jangstagram.dto.PostCreateRequestDto;
+import com.insta.jangstagram.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class PostService {
+
+    private final PostRepository postRepository;
+
+    public void write(PostCreateRequestDto postCreateRequestDto){
+        Post post = new Post(postCreateRequestDto.getTitle(), postCreateRequestDto.getContent());
+        postRepository.save(post);
+    }
 }
