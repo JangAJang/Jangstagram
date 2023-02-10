7583ee4db9335db81eac3c17da3d69054adc92a6
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-10 20:51:54 +0900

    feat: ErrorResponse 틀린 데이터가 무엇인지, 필드명과 메시지를 매핑하여 인스턴스에 포함하도록 기능 추가

--- src/main/java/com/insta/jangstagram/response/ErrorResponse.java
+++ src/main/java/com/insta/jangstagram/response/ErrorResponse.java
@@ -3,10 +3,19 @@ package com.insta.jangstagram.response;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Getter
 public class ErrorResponse {
 
     private final String code;
     private final String message;
+
+    private Map<String, String> validationField = new HashMap<>();
+
+    public void addValidation(String field, String defaultMessage) {
+        this.validationField.put(field, defaultMessage);
+    }
 }
