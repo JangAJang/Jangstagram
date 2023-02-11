03ffa9ac89228a539e59e0c8f17de40381bf2a68
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-11 23:54:38 +0900

    refactor: PostResponseDto 응답 dto생성시에 제목을 10글자이내로 응답하게 생성자 수정

--- src/main/java/com/insta/jangstagram/dto/PostResponseDto.java
+++ src/main/java/com/insta/jangstagram/dto/PostResponseDto.java
@@ -2,14 +2,18 @@ package com.insta.jangstagram.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@Builder
-@RequiredArgsConstructor
 public class PostResponseDto {
 
     private final Long id;
     private final String title;
     private final String content;
+
+    @Builder
+    public PostResponseDto(Long id, String title, String content) {
+        this.id = id;
+        this.title = title.substring(0, Math.min(10, title.length()));
+        this.content = content;
+    }
 }
