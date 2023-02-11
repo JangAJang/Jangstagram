37a0f6ec8be42f9cd5579e3d85af41f0e1841d00
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-11 16:19:47 +0900

    refactor: PostControllerTest json을 직접 작성하는게 아닌 json을 만들어주는 기능 구현

--- src/test/java/com/insta/jangstagram/controller/PostControllerTest.java
+++ src/test/java/com/insta/jangstagram/controller/PostControllerTest.java
@@ -1,6 +1,9 @@
 package com.insta.jangstagram.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.insta.jangstagram.domain.Post;
+import com.insta.jangstagram.dto.PostCreateRequestDto;
 import com.insta.jangstagram.repository.PostRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -41,10 +45,14 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts/add를 요청하면 Hello World가 출력된다. ")
     public void postTest() throws Exception{
+        //given
+        String json = makeJson(new PostCreateRequestDto("제목입니다", "내용입니다"));
+        //when
+
+        //then
         mvc.perform(post("/posts/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\" : \"제목입니다\"," +
-                                "\"content\" : \"내용입니다\" }"))
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
                 .andExpect(content().string("{}"))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -54,10 +62,14 @@ class PostControllerTest {
     @Test
     @DisplayName("")        
     public void testWithExceptionTitle() throws Exception{
+        //given
+        String json = makeJson(new PostCreateRequestDto("", ""));
+        //when
+
+        //ten
         mvc.perform(post("/posts/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\" : \"\"," +
-                                "\"content\" : \"\" }"))
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value("400")) // json값 테스트
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
@@ -70,12 +82,11 @@ class PostControllerTest {
     @DisplayName("전체 테스트를 수행하면, 총 2개의 데이터가 존재한다. ")
     public void dataInTest() throws Exception{
         //given
-
+        String json = makeJson(new PostCreateRequestDto("제목", "내용"));
         //when
         mvc.perform(post("/posts/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"title\" : \"제목\"," +
-                                "\"content\" : \"내용\" }"))
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
                 .andExpect(status().isOk())
                 .andDo(print());
         Post post = postRepository.findAll().get(0);
@@ -84,5 +95,13 @@ class PostControllerTest {
         assertThat(post.getTitle()).isEqualTo("제목");
         assertThat(post.getContent()).isEqualTo("내용");
         //유닛단위에서는 1개가 맞지만 전체 테스트시에는 2개가 등록된다.
+    }
+
+    private String makeJson(Object object){
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            return "";
+        }
     }
 }
