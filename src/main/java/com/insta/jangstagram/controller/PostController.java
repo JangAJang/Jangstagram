392d495cc66d3ccc9402f1764d404ef976aa53c6
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-11 19:46:38 +0900

    fix: PostController @Requestparam에 value추가

--- src/main/java/com/insta/jangstagram/controller/PostController.java
+++ src/main/java/com/insta/jangstagram/controller/PostController.java
@@ -28,7 +28,7 @@ public class PostController {
     }
 
     @GetMapping("/posts/{id}")
-    public PostCreateRequestDto getOne(@RequestParam Long id){
+    public PostCreateRequestDto getOne(@RequestParam(value = "id") Long id){
         return postService.getOne(id);
     }
 }
