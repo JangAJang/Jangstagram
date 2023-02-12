32794eadb2b4c1711be30e8441a54997b66fd771
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-12 23:16:04 +0900

    feat: PostCustomRepositoryImpl querydsl을 이용해 최근에 만들어진 게시물 순으로 페이징처리로 조회하는 로직 구현

--- src/main/java/com/insta/jangstagram/dto/PostResponseDto.java
+++ src/main/java/com/insta/jangstagram/dto/PostResponseDto.java
@@ -1,5 +1,6 @@
 package com.insta.jangstagram.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,6 +12,7 @@ public class PostResponseDto {
     private final String content;
 
     @Builder
+    @QueryProjection
     public PostResponseDto(Long id, String title, String content) {
         this.id = id;
         this.title = title.substring(0, Math.min(10, title.length()));
--- src/main/java/com/insta/jangstagram/repository/PostCustomRepositoryImpl.java
+++ src/main/java/com/insta/jangstagram/repository/PostCustomRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.insta.jangstagram.repository;
 import com.insta.jangstagram.domain.QPost;
 import com.insta.jangstagram.dto.PostResponseDto;
+import com.insta.jangstagram.dto.QPostResponseDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class PostCustomRepositoryImpl implements PostCustomRepository{
@@ -13,6 +17,23 @@ public class PostCustomRepositoryImpl implements PostCustomRepository{
 
     @Override
     public Page<PostResponseDto> getPage(Pageable pageable) {
-        return null;
+        return new PageImpl<PostResponseDto>(getData(pageable), pageable, getSize());
+    }
+
+    private List<PostResponseDto> getData(Pageable pageable){
+        return queryFactory
+                .select(new QPostResponseDto(QPost.post.id, QPost.post.title, QPost.post.content))
+                .from(QPost.post)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(QPost.post.id.desc())
+                .fetch();
+    }
+
+    private Long getSize(){
+        return queryFactory
+                .select(QPost.post.count())
+                .from(QPost.post)
+                .fetchOne();
     }
 }
