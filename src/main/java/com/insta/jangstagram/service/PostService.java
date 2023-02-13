390e46b4211f942746250a4940b32313641c29ac
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-13 17:14:00 +0900

    feat: PostService 게시글을 삭제하는 기능 구현

--- src/main/java/com/insta/jangstagram/service/PostService.java
+++ src/main/java/com/insta/jangstagram/service/PostService.java
@@ -67,6 +67,13 @@ public class PostService {
         post.edit(postEditor);
     }
 
+    public void delete(Long id){
+        Post post = postRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 글입니다"));
+
+        postRepository.delete(post);
+    }
+
     private static void editTitle(PostEditRequestDto postEdit, Post post, PostEditor.PostEditorBuilder postEditorBuilder) {
         if(postEdit.getTitle() == null) {
             postEditorBuilder.title(post.getTitle());
