d79ab1d714a1939f251a5a1083cc577fead0b56e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-13 17:44:57 +0900

    refactor: ErrorResponse 일반 예외발생시에 validationField가 불필요해 비어있는 값 제거하는 코드 추가

--- src/main/java/com/insta/jangstagram/response/ErrorResponse.java
+++ src/main/java/com/insta/jangstagram/response/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.insta.jangstagram.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Getter
 @Builder
+@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
 public class ErrorResponse {
 
     private final String code;
