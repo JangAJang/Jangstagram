c0ba7d630f0fd832270edf48256ed6d862e1090e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-13 15:24:13 +0900

    refactor: PostService 어노테이션 Transactional추가

--- src/main/java/com/insta/jangstagram/service/PostService.java
+++ src/main/java/com/insta/jangstagram/service/PostService.java
@@ -8,13 +8,11 @@ import com.insta.jangstagram.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -44,16 +42,19 @@ public class PostService {
                 .content(post.getContent()).build();
     }
 
+    @Transactional(readOnly = true)
     public List<PostResponseDto> getList(Pageable pageable) {
         return postRepository.findAll(pageable).stream()
                 .map(s -> new PostResponseDto(s.getId(), s.getTitle(), s.getContent()))
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public Page<PostResponseDto> getPage(Pageable pageable){
         return postRepository.getPage(pageable);
     }
 
+    @Transactional
     public void edit(Long id, PostEditRequestDto postEdit){
         Post post = postRepository.findById(id)
                 .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 글입니다"));
