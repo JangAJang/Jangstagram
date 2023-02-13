318908f61b201c0e0f8c6778426529f87014d509
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-13 21:49:28 +0900

    feat: PostControllerDocTest Rest Doc을 테스트하기 위해 인덱스 화면을 요청하는 컨트로럴 테스트로직 구현

--- src/test/java/com/insta/jangstagram/controller/PostControllerDocTest.java
+++ src/test/java/com/insta/jangstagram/controller/PostControllerDocTest.java
@@ -1,14 +1,22 @@
 package com.insta.jangstagram.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 
 @SpringBootTest
 @ExtendWith(RestDocumentationExtension.class)
@@ -21,5 +29,16 @@ public class PostControllerDocTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
                 .build();
+    }
+    
+    @Test
+    @DisplayName("인덱스 요청시에 정상동작하는가")
+    public void test1() throws Exception{
+        //given
+        
+        //expected
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("index"));
     }
 }
